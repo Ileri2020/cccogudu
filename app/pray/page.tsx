61b99d013f79519d462add6fe4f652d02790bbd1
Adepoju Ololade <adepojuololade2020@gmail.com>
2025-09-01 00:14:18 +0100

    video conference success

--- app/pray/page.tsx
+++ app/pray/page.tsx
@@ -13,11 +13,11 @@ import {
 import { Room, Track } from 'livekit-client';
 import '@livekit/components-styles';
 import { useEffect, useState } from 'react';
+import { Input } from '@/components/ui/input';
+import { Button } from '@/components/ui/button';
+
 
 export default function Pray() {
-  // TODO: get user input for room and name
-  const room = 'quickstart-room';
-  const name = 'quickstart-user';
   const [roomInstance] = useState(() => new Room({
     // Optimize video quality for each participant's screen
     adaptiveStream: true,
@@ -25,28 +25,33 @@ export default function Pray() {
     dynacast: true,
   }));
   const [token, setToken] = useState('');
+  const [room, setRoom] = useState('');
+  const [name, setName] = useState('');
+  const [isConnected, setIsConnected] = useState(false);
+
+  const fetchtoken = async () => {
+    try {
+      const resp = await fetch(`/api/token?room=${room}&username=${name}`);
+      const data = await resp.json();
+      console.log('data', `${data.token}`)
+      if (data.token) {
+        await roomInstance.connect(process.env.NEXT_PUBLIC_LIVEKIT_URL, `${data.token}`);
+        setIsConnected(true);
+      }
+      setToken(`${data.token}`)
+      console.log('room instance generated token variable set');
+    } catch (e) {
+      console.log('error generating token', e);
+    }
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    fetchtoken();
+  }
 
   useEffect(() => {
-    let mounted = true;
-    (async () => {
-      try {
-        const resp = await fetch(`/api/token?room=${room}&username=${name}`);
-        const data = await resp.json();
-        console.log('data', `${data.token}`)
-        if (!mounted) return;
-        if (data.token) {
-          await roomInstance.connect(process.env.NEXT_PUBLIC_LIVEKIT_URL, `${data.token}`);
-        }
-        setToken(`${data.token}`)
-        console.log('room instance generated token variable set');
-      } catch (e) {
-        //console.error(e);
-        console.log('error generating token', e);
-      }
-    })();
-  
     return () => {
-      mounted = false;
       roomInstance.disconnect();
     };
   }, [roomInstance]);
@@ -57,25 +62,40 @@ export default function Pray() {
         <div>buttons to schedule prayer meeting, public or private with selected accounts</div>
         <div>prayer schedules for accounts holders on the platform</div>
       </div>
-      {
-        (token === '') ?
-      <div>Getting schedule token...</div>
-      :
-      <div className='w-full flex justify-center items-center mx-auto'>
-        <div data-lk-theme="default" className='h-[50vh] max-w-3xl bg-secondary rounded-lg flex flex-col overflow-clip m-2'>
-          {/* Your custom component with basic video conferencing functionality. */}
-          <MyVideoConference />
-          {/* The RoomAudioRenderer takes care of room-wide audio for you. */}
-          <RoomAudioRenderer />
-          {/* Controls for the user to start/stop audio, video, and screen share tracks */}
-          <ControlBar />
-        </div>
+      <div>
+        <form onSubmit={handleSubmit} className='flex flex-col gap-2 p-2 m-2 bg-secondary w-[300px] rounded-lg'>
+          <Input
+            type="text"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+            placeholder="Name"
+          />
+          <Input
+            type="text"
+            value={room}
+            onChange={(e) => setRoom(e.target.value)}
+            placeholder="Room"
+          />
+          <Button type="submit">Join Room</Button>
+        </form>
       </div>
-      }
+      {((token === '')&& !isConnected) ? (
+        <div>Please join a room</div>
+      ) : (
+        <div className='w-full flex justify-center items-center mx-auto'>
+          <div data-lk-theme="default" className='h-[50vh] max-w-3xl bg-secondary rounded-lg flex flex-col overflow-clip m-2'>
+            {/* Your custom component with basic video conferencing functionality. */}
+            <MyVideoConference />
+            {/* The RoomAudioRenderer takes care of room-wide audio for you. */}
+            <RoomAudioRenderer />
+            {/* Controls for the user to start/stop audio, video, and screen share tracks */}
+            <ControlBar />
+          </div>
+        </div>
+      )}
     </RoomContext.Provider>
   );
 }
-
 
 
 
